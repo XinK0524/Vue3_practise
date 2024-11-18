b6e37b04b0968ba4e9f7030a7efda316de470220
XinK0524 <aea91268@gmail.com>
2024-11-18 21:28:27 +0800

    TS 與 prpos應用

---
+++ 備份/20_回顧TS/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <Preson />
+</template>
+
+<script lang="ts" setup name="App">
+    import Preson from './components/Preson.vue';
+</script>
---
+++ 備份/20_回顧TS/components/Preson.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="person">
+        ???
+    </div>
+</template>
+<script lang="ts" setup name="Person">
+    import {type PersonInter,type Persons} from "@/types"
+    
+    // let person:PersonInter = {id:"1111",name:'dog',age:80}
+    
+    let personList:Persons = [
+        {id:"1111",name:'dog',age:80},
+        {id:"2222",name:'cat',age:30},
+        {id:"3333",name:'book',age:60},
+    ]
+
+</script>
+
+<style scoped>
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        padding: 10px;
+        margin: 10px;
+    }
+</style>
