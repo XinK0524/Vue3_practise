ae874bf30e8d7f5e2cadf80e4acb1dda62b9a960
XinK0524 <aea91268@gmail.com>
2024-12-17 17:32:24 +0800

    route基本切換

---
+++ 備份/24_route基本切換/components/News.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="news">
+        <h2>你好這是新聞</h2>
+    </div>
+</template>
+
+<script setup lang="ts" name="News">
+</script>
+
+<style scoped>
+    .news h2{
+        text-align: center;
+        font-size: 40px
+    }
+</style>
