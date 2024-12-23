c3689922447c20ea51356b43d09d6fd56f90ac69
XinK0524 <aea91268@gmail.com>
2024-12-23 18:18:14 +0800

    路由params

---
+++ 備份/26_路由params/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2 class="title">路由測試</h2>
+</template>
+
+<script setup lang="ts" name="title">
+</script>
+
+<style scoped>
+    .title{
+        height: 70px;
+        background-color: bisque;
+        color: brown;
+        align-content: center;
+        text-align: center;
+    }
+</style>
---
+++ 備份/26_路由params/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home">
+        <h2>你好這是家</h2>
+    </div>
+</template>
+
+<script setup lang="ts" name="Home">
+</script>
+
+<style scoped>
+    .home h2{
+        text-align: center;
+        font-size: 40px
+    }
+</style>
